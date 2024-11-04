ae67855f475303ac579801c7041b70faca05dfcd
Ana <a.titarenko@hraymo.com>
2024-11-04 12:34:55 +0100

    Hw 1

---
+++ Hw_01.js
@@ -0,0 +1,27 @@
+let greeting = 'hello';
+let company = 'owu';
+let domain = 'com';
+let country = 'ua';
+let numberOne = 1;
+let numberTen = 10;
+let negativeNumber = -999;
+let positiveNumber = 123;
+let pi = 3.14;
+let eulerNumber = 2.7;
+let age = 16;
+let isTrue = true;
+let isFalse = false;
+
+console.log(greeting);       // 'hello'
+console.log(company);        // 'owu'
+console.log(domain);         // 'com'
+console.log(country);        // 'ua'
+console.log(numberOne);      // 1
+console.log(numberTen);      // 10
+console.log(negativeNumber); // -999
+console.log(positiveNumber); // 123
+console.log(pi);             // 3.14
+console.log(eulerNumber);    // 2.7
+console.log(age);            // 16
+console.log(isTrue);         // true
+console.log(isFalse);        // false
